356a80964f27220c539437cd0e84b9d723d76b7b
Alec Wolyniec <alecwolyniec@gmail.com>
2021-03-17 21:20:11 -0400

    feat: Add sound, copyright attribution, and square that changes into a circle

--- script.js
+++ script.js
@@ -6,4 +6,16 @@ window.onload = () => {
       block3.classList.remove('jump-up');
     }, 3000);
   });
+
+  const block2 = document.getElementById('block2');
+  const block2Song = document.getElementById('block2-song');
+  let audioOn = false;
+  block2.onclick = () => {
+    if (audioOn) {
+      block2Song.pause();
+    } else {
+      block2Song.play();
+    }
+    audioOn = !audioOn;
+  }
 }
